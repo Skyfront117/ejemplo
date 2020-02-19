c251dff07aab35049939582e71da3c93381ce679
Skyfront117 <guillemlali@gmail.com>
2020-02-19 12:16:04 +0100

    h

---
+++ Shoot'em Up 1/Assets/Scripts/cameraBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBehavior : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D rigidBody;
+    public float speed = 2.0f;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector2(speed * (Time.deltaTime * 1000), 0);
+    }
+}
---
+++ Shoot'em Up 1/Assets/Scripts/playerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public GameObject mainCamera;
+    private float verticalSpeed = 10.0f;
+    private float horizontalSpeed = 5.0f;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, verticalSpeed * (Time.fixedDeltaTime * 1000));
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, -verticalSpeed * (Time.fixedDeltaTime * 1000));
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidBody.velocity = new Vector2(horizontalSpeed * (Time.fixedDeltaTime * 1000), rigidBody.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rigidBody.velocity = new Vector2(-horizontalSpeed * (Time.fixedDeltaTime * 1000), rigidBody.velocity.y);
+        }
+        else 
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+    }
+}
